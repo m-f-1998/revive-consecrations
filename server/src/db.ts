808e4b24eae7642cf1195707d02d2a3137712070
Matthew Frankland <admin@matthewfrankland.co.uk>
2025-08-29 20:14:51 +0100

    Updates database environment variables
    Updates the names of the database environment variables
    to align with convention and improve clarity.
    
    This change ensures consistency between the docker compose
    file and the database connection configuration.

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg"
 export const pool = new Pool ( {
   host: "db",
   port: 5432,
-  user: process.env [ "USER" ] || "youruser",
-  password: process.env [ "PASS" ] || "yourpass",
-  database: process.env [ "DB" ] || "yourdb",
+  user: process.env [ "POSTGRES_USER" ] || "youruser",
+  password: process.env [ "POSTGRES_PASSWORD" ] || "yourpass",
+  database: process.env [ "POSTGRES_DB" ] || "yourdb",
 } )
