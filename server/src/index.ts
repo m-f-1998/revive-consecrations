8e6932085bbab0726f17f08bbc228ce0f4375e93
Matthew Frankland <admin@matthewfrankland.co.uk>
2025-12-14 21:55:52 +0000

    CSP Update

--- server/src/index.ts
+++ server/src/index.ts
@@ -73,6 +73,7 @@ app.use ( helmet ( {
         "'self'",
         "https://\*.google-analytics.com",
         "https://\*.google.com",
+        "https://cloudflareinsights.com"
       ],
       frameSrc: [
         "'self'",
